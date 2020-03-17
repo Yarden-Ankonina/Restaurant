d5b157e63fa47ba44af93dc6b86cc5234941bf18
Yarden Ankonina <yardi1994@gmail.com>
2020-03-17 02:31:12 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
     
     // declare variables
+    //navbar
     const logo = document.getElementById('logo');
     const home = document.getElementsByClassName('home__button');
+    const infoNavbar = document.getElementsByClassName("infoNavbar")[0];
+    //collapse
+    const menuBar = document.getElementsByClassName("bar__icon")[0];
+    const exitCollapse = document.getElementsByClassName("collapse__exit")[0];
+    const collapseMode = document.getElementsByClassName("collapseMode")[0];
+    collapseMode.style.display = "none";
+    //info
     const galleryArrowLeft = document.getElementsByClassName("gallery__arrow")[0];
     const galleryArrowRight = document.getElementsByClassName("gallery__arrow")[1];
     const galleryPhotos = {
@@ -14,8 +22,8 @@
     }
     let galleryIndex = 1;
     let galleryImg = document.getElementsByClassName("gallery__img")[0];
-    const infoNavbar = document.getElementsByClassName("infoNavbar")[0];
     let info = document.getElementsByClassName("info")[0];
+
 
     //navbar
     logo.addEventListener('click',function(){
@@ -26,6 +34,22 @@
         window.location.reload();
     });
 
+    //collapse
+
+    menuBar.addEventListener('click',function(){
+        collapseMode.style.display = "block";
+        document.getElementsByClassName("notNavBar")[0].style.display = "none";
+        
+    });
+
+    exitCollapse.addEventListener('click', function(){
+        collapseMode.style.display = "none";
+        document.getElementsByClassName("notNavBar")[0].style.display = "block";
+    });
+    //location
+
+    //<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.3133700018475!2d35.18729526407921!3d31.786389347115794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502d64bb00558d3%3A0x14b067e2e1f1c99!2sShatner%20St%205%2C%20Jerusalem!5e0!3m2!1sen!2sil!4v1584313985992!5m2!1sen!2sil" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
     // info
     info.style.display = "none";
 
@@ -33,6 +57,7 @@
     info.style.display = "flex";
     });
 
+    
 
     galleryArrowLeft.addEventListener('click',function(){
         if(galleryIndex === 1){
